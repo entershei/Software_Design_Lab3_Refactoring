218295a6584f6cdd0fbfacad49f31f822cd6b9f4
Irina <entershei@gmail.com>
2020-12-24 00:02:34 +0700

    change exception behaviour

--- src/main/java/refactoring/html/HtmlManager.java
+++ src/main/java/refactoring/html/HtmlManager.java
@@ -2,33 +2,26 @@ package refactoring.html;
 
 import java.io.PrintWriter;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class HtmlManager {
 
-    public static String printSingleInt(ResultSet rs) {
-        try {
-            if (rs.next()) {
-                return rs.getInt(1) + "\n";
-            } else {
-                return "";
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    public static String printSingleInt(ResultSet rs) throws SQLException {
+        if (rs.next()) {
+            return rs.getInt(1) + "\n";
+        } else {
+            return "";
         }
     }
 
-    public static String printAllEntries(ResultSet rs) {
+    public static String printAllEntries(ResultSet rs) throws SQLException {
         StringBuilder sb = new StringBuilder();
 
-        try {
-            while (rs.next()) {
-                String name = rs.getString("name");
-                int price = rs.getInt("price");
-                sb.append(name).append("\t").append(price).append("</br>\n");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        while (rs.next()) {
+            String name = rs.getString("name");
+            int price = rs.getInt("price");
+            sb.append(name).append("\t").append(price).append("</br>\n");
         }
 
         return sb.toString();
